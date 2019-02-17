c158b7b91098d30dd16af06a5eaa74818a528b98
Lullaby16 <samuelys945@gmail.com>
2019-02-17 11:40:28 +0700

    upload Profil

---
+++ js/script.js
@@ -0,0 +1,61 @@
+//event pada saat di klik
+$('.page-scroll').on('click', function(e){
+
+var tujuan = $(this).attr('href');
+
+var elemenTujuan = $(tujuan);
+
+//pindahkan scroll
+$('body').animate({
+	scrollTop: elemenTujuan.offset().top - 50
+}, 1250);
+e.preventDefault();
+});
+
+
+
+//paralax
+//about
+$(window).on('load', function(){
+	$('.pkiri').addClass('pMuncul');
+	$('.pkanan').addClass('pMuncul');
+});
+
+
+$(window).scroll(function() {
+	var wScroll = $(this).scrollTop();
+	
+	//jumbotron
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+	});
+	
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+wScroll/1.2 +'%)'
+	});
+	
+
+	//portfolio
+	if( wScroll > $('.portfolio').offset().top - 250 ) {
+		$('.portfolio .thumbnail').each(function(i) {
+			setTimeout(function() {
+				$('.portfolio .thumbnail').eq(i).addClass('keluar');
+			}, 300 * (i+1) );
+		});
+		
+		
+	}
+
+
+
+
+});
+
+
+
+
+
